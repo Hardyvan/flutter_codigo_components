a25895819b18b2423c8caa5d6dc528435fd68ff9
Hardyvan <velezcruzi@gmail.com>
2025-04-02 17:43:10 -0500

    Avances del día

--- lib/pages/alert_page.dart
+++ lib/pages/alert_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class AlertPage extends StatelessWidget {
   showMyAlert(BuildContext mandarina) {
@@ -12,13 +13,89 @@ class AlertPage extends StatelessWidget {
             "HOla este es el contenido de mi alert, este es texto de prueba",
           ),
           actions: [
-            TextButton(onPressed: () {
-              Navigator.pop(context);
-            }, child: Text("Cancelar",),),
-            TextButton(onPressed: () {
-
-            }, child: Text("Aceptar",),),
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text("Cancelar"),
+            ),
+            TextButton(onPressed: () {}, child: Text("Aceptar")),
           ],
+        );
+      },
+    );
+  }
+
+  showMyAlert2(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          contentPadding: EdgeInsets.all(8.0),
+
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(14.0),
+          ),
+          content: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                "Hola a todos",
+                style: GoogleFonts.poppins(
+                  fontWeight: FontWeight.w600,
+                  color: Colors.black87,
+                ),
+              ),
+              const Divider(thickness: 0.7),
+              const SizedBox(height: 6.0),
+              const CircleAvatar(
+                backgroundColor: Colors.black12,
+                backgroundImage: NetworkImage(
+                  "https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(Icons.star, color: Colors.amber),
+                  Icon(Icons.star, color: Colors.amber),
+                  Icon(Icons.star, color: Colors.amber),
+                  Icon(Icons.star, color: Colors.amber),
+                  Icon(Icons.star, color: Colors.amber),
+                ],
+              ),
+              const SizedBox(height: 6.0),
+
+              Text(
+                "Hola de Nuevo, este es el content, vamos a seguir practicando",
+                textAlign: TextAlign.center,
+                style: GoogleFonts.poppins(
+                  color: Colors.black54,
+                  fontSize: 13.0,
+                ),
+              ),
+
+              const SizedBox(height: 10.0),
+
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+
+                children: [
+
+                  TextButton(
+                    onPressed: () {},
+                    child: Text("Cancelar"),
+                  ),
+                  const SizedBox(width: 4.0),
+                  TextButton(
+                    onPressed: () {},
+                    child: Text("Aceptar"),
+                  ),
+                ],
+              ),
+            ],
+          ),
         );
       },
     );
@@ -40,6 +117,12 @@ class AlertPage extends StatelessWidget {
                 showMyAlert(context);
               },
               child: Text("Aler 1"),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                showMyAlert2(context);
+              },
+              child: Text("Aler 2"),
             ),
           ],
         ),
